7a965004d1012da259e7b64cf7b696827d738713
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-10-27 03:30:56 -0500

    update up_test.sh

--- up_test.sh
+++ up_test.sh
@@ -1,1 +1,1 @@
-docker-compose down -v && docker-compose --env-file .env.test -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from node-app-test
+docker-compose -f docker-compose.test.yml down -v && docker-compose --env-file .env.test -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from node-app-test
